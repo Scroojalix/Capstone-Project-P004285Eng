0cb17a57d0fbbfc026e18e75337a0f581a076db5
Scroojalix <scroojalixyt@gmail.com>
2026-06-27 01:08:52 +1000

    Removed parameter bridge yaml config file, as no longer needed.

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
         (os.path.join('share', package_name, 'worlds'), glob('worlds/*.sdf')),
         (os.path.join('share', package_name, 'models'), glob('models/*.sdf')),
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
